55abad30c53deb29f2264fb803d1e2ca87e72d8e
SaToGo1 <stoledano97@gmail.com>
2023-09-11 00:15:43 +0200

    INTRO: native modules Path, correct

--- 01.Intro/5.path.js
+++ 01.Intro/5.path.js
@@ -5,3 +5,17 @@ const path = require('node:path')
 // -> windows \
 
 console.log(path.sep)
+
+// unir rutas con path.join
+const filePath = path.join('content', 'subfolder', 'test.txt')
+// `/content/subfolder/test.txt`
+console.log(filePath)
+
+const base = path.basename('/tmp/secret-files/passwords.txt')
+console.log(base)
+
+const filename = path.basename('/tmp/secret-files/passwords.txt', '.txt')
+console.log(filename)
+
+const extension = path.extname('image.jpg')
+console.log(extension)
