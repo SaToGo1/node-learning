32c07c218b4c3edf86e0135dce492aa74ce5f7f4
SaToGo1 <stoledano97@gmail.com>
2023-11-03 19:17:07 +0100

    2.API EXPRESS: Ruta de imagen

--- 02.API_express/1.http.js
+++ 02.API_express/1.http.js
@@ -1,12 +1,32 @@
 const http = require('node:http')
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 3000
 
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8') // text/plain, text/html, application/json
+
   if (req.url === '/') {
     res.statusCode = 200 // OK
-    res.setHeader('Content-Type', 'text/plain') // text/plain, text/html, application/json
-    res.end('Bienvenido a mi página de inicio')
+    res.end('<h1>Bienvenido a mi página de inicio</h1>')
+  } else if (req.url === '/imagen.png') {
+    res.statusCode = 200 // OK
+    fs.readFile('./esquema.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internal Server Error</h1>')
+      }
+
+      console.log('gets here')
+      res.setHeader('Content-Type', 'image/png')
+      res.end(data)
+    })
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200 // OK
+    res.end('<h1>Contacto</h1>')
+  } else {
+    res.statusCode = 404 // NOT FOUND
+    res.end('<h1>404 Page Not Found</h1>')
   }
 }
 
