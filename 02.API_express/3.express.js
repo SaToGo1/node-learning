06b2bd3f86e1267a7f9392426287a19f92065ea6
SaToGo1 <stoledano97@gmail.com>
2023-11-03 22:43:43 +0100

    2.API EXPRESS: API Express-> POST

--- 02.API_express/3.express.js
+++ 02.API_express/3.express.js
@@ -1,10 +1,34 @@
 const express = require('express')
-const app = express()
+const ditto = require('./pokemon/ditto.json')
 
 const PORT = process.env.PORT ?? 3000
 
-app.get('/', (req, res) => {
-  res.send('<h1>Mi página</h1>')
+const app = express()
+app.disable('x-powered-by')
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
+})
+
+app.post('/pokemon', (req, res) => {
+  let body = ''
+
+  // escuchar el evento
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+
+    data.timestamp = Date.now()
+    res.status(201).json(data)
+  })
+})
+
+// la última a la que va a llegar
+app.use((req, res) => {
+  res.status(404).send('<h1>404 Not Found</h1>')
 })
 
 app.listen(PORT, () => {
