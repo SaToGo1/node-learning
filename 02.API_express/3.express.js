b72c23c382e81e47f05b9128dc5edaefee3f2fda
SaToGo1 <stoledano97@gmail.com>
2023-11-03 23:13:23 +0100

    2.API EXPRESS: API Express-> Middleware para POST json

--- 02.API_express/3.express.js
+++ 02.API_express/3.express.js
@@ -6,24 +6,38 @@ const PORT = process.env.PORT ?? 3000
 const app = express()
 app.disable('x-powered-by')
 
+app.use(express.json())
+// El middelware anterior have lo mismo que el siguiente comentado
+
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   // solo llegan request que son POST y que tienen el header Content-Type: application/json
+//   let body = ''
+
+//   // escuchar el evento
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la informaci'on en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
+
 app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
 })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
-
-  // escuchar el evento
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-
-  req.on('end', () => {
-    const data = JSON.parse(body)
-
-    data.timestamp = Date.now()
-    res.status(201).json(data)
-  })
+  // req.body deberíamos guardar en bbdd
+  res.status(201).json(req.body)
 })
 
 // la última a la que va a llegar
