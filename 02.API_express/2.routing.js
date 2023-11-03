ed3f00a31ee4d33deb7c89970c46ab3981faef48
SaToGo1 <stoledano97@gmail.com>
2023-11-03 19:28:09 +0100

    2.API EXPRESS: API first get route

--- 02.API_express/2.routing.js
+++ 02.API_express/2.routing.js
@@ -1,7 +1,24 @@
 const http = require('node:http') // protocolo HTTP
 
+// commonJS -> módulos clásicos de node puedes importar json automaticamente
+const dittoJSON = require('./pokemon/ditto.json')
+
 const processRequest = (req, res) => {
   const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404 Not Found</h1>')
+      }
+  }
 }
 
 const server = http.createServer(processRequest)
