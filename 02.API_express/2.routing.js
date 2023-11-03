3bbdd42f80b58cda623066d86b606069bd086c57
SaToGo1 <stoledano97@gmail.com>
2023-11-03 20:23:57 +0100

    2.API EXPRESS: API POST routes + api.http

--- 02.API_express/2.routing.js
+++ 02.API_express/2.routing.js
@@ -18,6 +18,34 @@ const processRequest = (req, res) => {
           res.setHeader('Content-Type', 'text/html; charset=utf-8')
           return res.end('<h1>404 Not Found</h1>')
       }
+
+    case 'POST':
+      switch (url) {
+        case '/pokemon': {
+          let body = ''
+
+          // escuchar el evento
+          req.on('data', chunk => {
+            body += chunk.toString()
+          })
+
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            // llamar a una base de datos para guardar la info
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+
+            data.timestamp = Date.now()
+            res.end(JSON.stringify(data))
+          })
+
+          break
+        }
+
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+          res.end('404 Not Found')
+      }
   }
 }
 
