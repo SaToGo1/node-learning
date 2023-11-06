bd90891e4a7ff8c8e1eab91025fd055d3ff77dc5
SaToGo1 <stoledano97@gmail.com>
2023-11-06 23:32:20 +0100

    3.API REST: GET /movies

--- 03.API_REST/app.js
+++ 03.API_REST/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const movies = require('./movies.json')
 
 const app = express()
 app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hola Mundo' })
+})
+
+// Todos los recursos que sean MOVIES se identifican con /movies
+app.get('/movies', (req, res) => {
+  res.json(movies)
 })
 
 const PORT = process.env.PORT ?? 3000
