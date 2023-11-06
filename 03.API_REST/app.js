03b4c36361d3787d4812106cad6e4687a38b1d23
SaToGo1 <stoledano97@gmail.com>
2023-11-07 00:07:29 +0100

    3.API REST: GET movies by id

--- 03.API_REST/app.js
+++ 03.API_REST/app.js
@@ -13,6 +13,14 @@ app.get('/movies', (req, res) => {
   res.json(movies)
 })
 
+app.get('/movies/:id', (req, res) => { // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
+})
+
 const PORT = process.env.PORT ?? 3000
 app.listen(PORT, () => {
   console.log(`open server in http://localhost:${PORT}`)
